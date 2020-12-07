df32ae73962199e2447e69e264512249b6ca6386
Eduard <eduardstancu@yahoo.com>
2020-12-08 00:40:14 +0200

    les76S5 withRouter

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,24 +11,26 @@ class Directory extends Component {
           title: 'hats',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
           id: 1,
-          linkUrl: 'shop/hats',
+          size: '',
+          linkUrl: 'hats',
         },
         {
           title: 'jackets',
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
           id: 2,
+          size: '',
           linkUrl: 'shop/jackets',
         },
         {
           title: 'sneakers',
           imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
           id: 3,
+          size: '',
           linkUrl: 'shop/sneakers',
         },
         {
           title: 'womens',
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          size: 'large',
           id: 4,
           size: 'large',
           linkUrl: 'shop/womens',
@@ -36,7 +38,6 @@ class Directory extends Component {
         {
           title: 'mens',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          size: 'large',
           id: 5,
           size: 'large',
           linkUrl: 'shop/mens',
@@ -48,15 +49,8 @@ class Directory extends Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map((section, index) => (
-          <MenuItem
-            key={index}
-            title={section.title}
-            subtitle='Shop Now'
-            imageUrl={section.imageUrl}
-            size={section.size}
-            id={section.id}
-          />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
