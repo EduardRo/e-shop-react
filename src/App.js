423af3332ecaee93f237af795fab1a05630db41e
Eduard <eduardstancu@yahoo.com>
2020-12-09 15:13:03 +0200

    Sls79 Collections implemented

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import './App.css';
 
 const HatsPage = () => {
   return <div>hats page</div>;
 };
+const JacketsPage = () => <div>Jackets Page</div>;
+const SneakersPage = () => <div>Sneakers Page</div>;
+
+const WomenPage = () => <div>Women Page</div>;
+const MenPage = () => <div>Men Page</div>;
 
 function App() {
   return (
-    <div className='App'>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/hats' component={HatsPage} />
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/jackets' component={JacketsPage} />
+        <Route exact path='/sneakers' component={SneakersPage} />
+        <Route exact path='/women' component={WomenPage} />
+        <Route exact path='/men' component={MenPage} />
+      </Switch>
     </div>
   );
 }
