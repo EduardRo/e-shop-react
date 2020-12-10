dced26438ae8d72219f156f5839be51be35175f9
Eduard <eduardstancu@yahoo.com>
2020-12-10 13:35:10 +0200

    S6l80 creating Header Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 import './App.css';
 
 const HatsPage = () => {
@@ -16,6 +17,7 @@ const MenPage = () => <div>Men Page</div>;
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
